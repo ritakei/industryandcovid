2e30f08db47c778a30295db8c254a3ff9c969b92
rtakei <rtakei@sciencepark.co.jp>
2022-07-11 10:45:12 +0900

    a

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="VisitorsandCOVID",
-    version="0.0.1",
+    name="industryandcovid",
+    version="0.0.3",
     author="rinko takei",
     author_email="rtakei@sciencepark.co.jp",
-    description="This project compares the number of amusement park visitors with COVID infected people.",
+    description="This project will compare amusement park sales with Internet industry sales and the number of people infected with COVID.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ritakei/VisitorsandCOVID",
+    url="https://github.com/ritakei/industryandcovid",
     project_urls={
-        "Bug Tracker": "https://github.com/ritakei/VisitorsandCOVID",
+        "Bug Tracker": "https://github.com/ritakei/industryandcovid",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -26,7 +26,7 @@ setuptools.setup(
     python_requires=">=3.8.5",
     entry_points = {
         'console_scripts': [
-            'industoryandcovid = industoryandcovid:main'
+            'industryandcovid = industryandcovid:main'
         ]
     },
 )
